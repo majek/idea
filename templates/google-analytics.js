fe1baf3965e6350ada01dd6cde3a7f5b862c18f3
Marek Majkowski <majek04@gmail.com>
2012-06-05 17:01:04 +0100

    google analytics

---
+++ templates/google-analytics.js
@@ -0,0 +1,17 @@
+
+var _gaq = [];
+if (document.location.href.indexOf('idea.popcount.org') !== -1) {
+
+    // I surrender my soul to Google. Feel encouraged to use noscript.
+    _gaq = [['_setAccount', 'UA-283061-10'], ['_trackPageview']];
+
+    (function() {
+        var ga = document.createElement('script');
+        ga.type = 'text/javascript';
+        ga.async = true;
+        ga.src = 'https://ssl.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(ga, s);
+    })();
+}
+

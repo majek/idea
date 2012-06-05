4d2b02ee7a1df011b4c2428f48b1734fa1c3577b
Marek Majkowski <majek04@gmail.com>
2012-06-05 20:17:10 +0100

    move reconnect to footer, also, don't hook ws on real deployment

--- templates/reconnect.js
+++ templates/reconnect.js
@@ -1,26 +1,28 @@
-// reconnect.js
 
-var ws_url = function(){
-    return ((document.location.protocol === 'http:') ? 'ws://' : 'wss://') +
-        document.location.host + '/ws';
-}
+if (document.location.href.indexOf('idea.popcount.org') === -1) {
 
-var try_reconnect = function(){
-    console.log('[*] reconnecting...');
-    setTimeout(function(){
-        ws = new WebSocket(ws_url());
-        ws.onclose = try_reconnect;
-        ws.onopen = function(){
-            ws.onclose = null;
-            document.location.reload();
-        };
-    }, 150);
-};
+    var ws_url = function(){
+        return ((document.location.protocol === 'http:') ? 'ws://' : 'wss://') +
+            document.location.host + '/ws';
+    }
 
-if ('WebSocket' in window && 'console' in window){
-    var ws = new WebSocket(ws_url());
-    ws.onopen = function(){
-        console.log('[*] ws hooked! ' + Date());
-        ws.onclose = try_reconnect;
+    var try_reconnect = function(){
+        console.log('[*] reconnecting...');
+        setTimeout(function(){
+            ws = new WebSocket(ws_url());
+            ws.onclose = try_reconnect;
+            ws.onopen = function(){
+                ws.onclose = null;
+                document.location.reload();
+            };
+        }, 150);
     };
+
+    if ('WebSocket' in window && 'console' in window){
+        var ws = new WebSocket(ws_url());
+        ws.onopen = function(){
+            console.log('[*] ws hooked! ' + Date());
+            ws.onclose = try_reconnect;
+        };
+    }
 }
